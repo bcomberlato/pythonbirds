dbe7c08a8f7780a857d6dcf8bd55159ea07ec9a2
bruno <bcomberlato@gmail.com>
2021-10-22 20:18:40 -0300

    criados atributos de instância na classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 27):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá{id(self)}'
 if __name__ == '__main__':
@@ -6,3 +10,7 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar()) #mais usual
+    print(p.nome)
+    p.nome="Bruno"
+    p.idade = 27
+    print(p.idade)
