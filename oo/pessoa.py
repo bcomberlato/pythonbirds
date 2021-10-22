6dceaeeddf8e326e6a21753f90d5a90363c13c54
bruno <bcomberlato@gmail.com>
2021-10-22 20:54:00 -0300

    criado atributo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self,*filhos , nome = None, idade = 27):
         self.idade = idade
         self.nome = nome
@@ -11,10 +13,13 @@ if __name__ == '__main__':
 
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')
+    luciano.sobrenome = 'Ramalho'
     #print(Pessoa.cumprimentar(p))
     #print(id(p))
     #print(p.cumprimentar()) mais usual
     for filho in luciano.filhos:
         print(filho.nome)
-
-
+    print(Pessoa.olhos)
+    print(renzo.olhos)
+    print(luciano.olhos)
+    print(luciano.sobrenome)
